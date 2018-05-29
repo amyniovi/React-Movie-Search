cc9ea2148de415a65e230a33cd1a15cd9bb7847e
Amy Niovi <amyniovi@gmail.com>
2018-05-29 15:27:48 +0100

    prettier code, ESLint and Prettier plugin

--- src/App.js
+++ src/App.js
@@ -6,95 +6,80 @@ import $ from 'jquery'
 
 class App extends Component {
 
-  constructor(props){
-      super(props);
-      this.state={
-        rows : [],
-        search: ''
-      }
-      
-      // const movies = [
-      // {id:0, title:"Avatar", src:"https://www.verdict.co.uk/wp-content/uploads/2017/10/Avatar-1440x810.jpg",overview:"On the lush alien world of Pandora live the Na'vi, beings who appear primitive but are highly evolved. Because the planet's environment is poisonous, human/Na'vi hybrids, called Avatars, must link to human minds to allow for free movement on Pandora"},
-      // {id:1, title:"Rampage",src:"https://thespinoff.scdn5.secure.raxcdn.com/wp-content/uploads/2018/04/Rampage-chinese-poster-with-Dwayne-Johnson.jpg", overview:"Global icon Dwayne Johnson headlines the action adventure “Rampage,” directed by Brad Peyton.  But a rogue genetic experiment gone awry mutates this gentle ape into a raging creature of enormous size. "}
-      // ];
-      
-      // var movieRows = [];
-    
-      // movies.forEach(movie=>{
-      //   const movieRow = <MovieRow movie={movie}/>
-      //   movieRows.push(movieRow)
-      // });
+  constructor(props) {
+    super(props);
+    this.state = {
+      rows: []
+    }
 
-      this.search()
-      this.updateSearch = this.updateSearch.bind(this);
+    this.search("a")
+    this.updateSearch = this.updateSearch.bind(this);
   }
 
-  search(search){
-    const urlStr= `https://api.themoviedb.org/3/search/movie?api_key=5e51b3d3d888bc3f1cfd2ccab3235bb2&query=${search}&page=1`
-    var movieRows=[]
-      $.ajax({
-        url:urlStr, 
-        success: result=>{
-          console.log("fetched data")
-          const movies = result.results
-          console.log(movies)
-          const movieRows = movies.map(movie=><MovieRow movie={movie}/>);
-          
-          this.setState({
-            rows :movieRows
-          })
-         
-          
-        },  
-        error:(xhr, status, err)=>{
-          console.error("failed")
-        }
-      } 
+  search(search) {
+    const urlStr = `https://api.themoviedb.org/3/search/movie?api_key=5e51b3d3d888bc3f1cfd2ccab3235bb2&query=${search}&page=1`
+    var movieRows = []
+    $.ajax({
+      url: urlStr,
+      success: result => {
+        console.log("fetched data")
+        const movies = result.results
+        console.log(movies)
+        const movieRows = movies.map(movie => <MovieRow movie={movie} />);
+
+        this.setState({
+          rows: movieRows
+        })
+      },
+      error: (xhr, status, err) => {
+        console.error("failed")
+      }
+    }
     )
-    
+
   }
 
   updateSearch(e) {
-   
+
     this.search(e.target.value);
   }
 
   render() {
 
     return (
-      <div >        
-       <table className="titleBar">
-         <tbody>
-           <tr>
-             <td>
-               <img border="1px" width="150"  height="150" src="green-app-logo.png"/>
-               </td>
-             <td width="200">
-            <h3> Search Torrents </h3>
-             </td>
-             </tr>
-         </tbody>
-       </table>
-      <input
-        onChange={this.updateSearch} 
-        style={{
-          fontSize:16,
-          display:'block',
-          width:"97%",
-          paddingTop:8,
-          paddingLeft:16,
-          color:"#000",
-          fontWeight:"bold"
-        }} placeholder="Search Movie/Actor/Genre"/>
+      <div >
+        <table className="titleBar">
+          <tbody>
+            <tr>
+              <td>
+                <img border="1px" width="150" height="150" src="green-app-logo.png" />
+              </td>
+              <td width="200">
+                <h3> Search Torrents </h3>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <input
+          onChange={this.updateSearch}
+          style={{
+            fontSize: 16,
+            display: 'block',
+            width: "97%",
+            paddingTop: 8,
+            paddingLeft: 16,
+            color: "#000",
+            fontWeight: "bold"
+          }} placeholder="Search Movie/Actor/Genre" />
 
-       {this.state.rows}
+        {this.state.rows}
 
       </div>
-          );
+    );
 
-     
+
   }
-  
+
 }
 
 export default App;
