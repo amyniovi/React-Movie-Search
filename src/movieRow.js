00070e738f03e36b77d50ee8d176c707b16228ef
Amy Niovi <amyniovi@gmail.com>
2018-05-29 13:35:52 +0100

    Complete React Movie Search  app

--- src/movieRow.js
+++ src/movieRow.js
@@ -1,21 +1,21 @@
-import React from 'react'
+import React from "react";
 
-class MovieRow extends React.Component{
-
-render(){
-
-return  <table key={this.props.movie.id}>
-<tr>
-  <td><img width="200" src={this.props.movie.src}/></td>
-  <td text-align="left">{this.props.movie.title} 
-
-  <p>{this.props.movie.overview}</p>
-  </td>
-</tr>    
-</table>
-
+class MovieRow extends React.Component {
+  render() {
+    return (
+      <table key={this.props.movie.id}>
+        <tr>
+          <td>
+            <img width="200" src={"https://image.tmdb.org/t/p/w185/" +this.props.movie.poster_path} />
+          </td>
+          <td text-align="left">
+            {this.props.movie.title}
+            <p>{this.props.movie.overview}</p>
+          </td>
+        </tr>
+      </table>
+    );
+  }
 }
 
-}
-
-export default MovieRow
+export default MovieRow;
