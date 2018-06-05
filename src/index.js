668286c63f6bfe69440959718ebf35455ba60a06
Amy Niovi <amyniovi@gmail.com>
2018-06-05 11:43:40 +0100

    Basic login Routing added

--- src/index.js
+++ src/index.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Link, Route,Switch } from 'react-router-dom';
 import './index.css';
 import App from './components/App.js';
 import registerServiceWorker from './registerServiceWorker';
 import HomePage from './components/pages/homePage';
+import LoginPage from './components/pages/loginPage';
+
+const Public = () =>
+(<div>
+    <ul>
+        <li><Link to='/home'>Search Torrents</Link></li>
+        <li><Link to='/login'>Sign In</Link></li>
+        {/* <li><Link to='/signup'>Sign Up</Link></li> */}
+    </ul>
+</div>);
+
+
+const PrivateRoute = ({ component, ...rest }) => {
+    return component;
+};
 
 ReactDOM.render(
     <BrowserRouter>
-        <HomePage />
+<Switch>
+        <Route exact path='/' component={Public} />
+        <Route path='/login' component={LoginPage} />
+        <PrivateRoute path='/home' component={HomePage} />
+        </Switch>
+
+
     </BrowserRouter>,
-     document.getElementById('root')
-    );
+    document.getElementById('root')
+);
 
 registerServiceWorker();
